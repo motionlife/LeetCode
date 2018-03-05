343031beed1ca09f8cb9518a0daaa9514fe638a6
Xiong <hxx160130@utdallas.edu>
2018-03-04 23:13:28 -0600

    Initial commit

---
+++ src/AddTwoNumbers.java
@@ -0,0 +1,34 @@
+/**
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ * <p>
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * <p>
+ * Example
+ * <p>
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ */
+public class AddTwoNumbers {
+
+    static int carry = 0;
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if(l1==null && l2==null & carry==0) return null;
+        ListNode node = new ListNode(0);
+        int sum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
+        node.val = sum % 10;
+        carry = sum / 10;
+        node.next = addTwoNumbers(l1 == null ? null : l1.next, l2 == null ? null : l2.next);
+        return node;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
---
+++ src/TwoSum.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * Given nums = [2, 7, 11, 15], target = 9,Because nums[0] + nums[1] = 2 + 7 = 9, return [0, 1].
+ */
+public class TwoSum {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{3,2,4};
+        int target =  6;
+        int[] result  = new TwoSum().twoSum(nums,target);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] copy = new int[nums.length];
+        System.arraycopy(nums,0,copy,0,nums.length);
+        Arrays.sort(copy);
+        int i = 0;
+        int j = copy.length - 1;
+        while (true){
+            int sum = copy[i] + copy[j];
+            if(sum>target) {
+                j--;
+            }
+            else if (sum<target){
+                i++;
+            }else{
+                break;
+            }
+        }
+        int start = 0, end = nums.length - 1;
+        while(nums[start]!=copy[i]) start++;
+        while (nums[end]!=copy[j]) end--;
+        return new int[]{start,end};
+    }
+}
