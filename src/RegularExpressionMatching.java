a3a7d20958de1489c16948f2d69712c70c871681
hxx160130 <+Xh547188+>
2018-03-19 21:49:37 -0500

    Solve more problems

--- src/RegularExpressionMatching.java
+++ src/RegularExpressionMatching.java
@@ -19,4 +19,28 @@
  * isMatch("aab", "c*a*b") → true
  */
 public class RegularExpressionMatching {
+    public static void main(String[] args) {
+        System.out.println(new RegularExpressionMatching().isMatch("aaa", "ab*a*c*a"));
+    }
+
+    public boolean isMatch(String s, String p) {
+        boolean[] match = new boolean[s.length() + 1];
+        match[s.length()] = true;
+
+        for (int i = p.length() - 1; i >= 0; i--) {
+            if (p.charAt(i) == '*') {
+                for (int j = s.length() - 1; j >= 0; j--) {
+                    match[j] = match[j] || (match[j + 1] && (p.charAt(i - 1) == '.' || p.charAt(i - 1) == s.charAt(j)));
+                }
+                i--;
+            } else {
+                for (int j = 0; j < s.length(); j++) {
+                    match[j] = match[j + 1] && (p.charAt(i) == '.' || p.charAt(i) == s.charAt(j));
+                }
+                match[s.length()] = false;
+            }
+        }
+
+        return match[0];
+    }
 }
